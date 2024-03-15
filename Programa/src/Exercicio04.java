d02f0fad2dd283fff0e64e01325186aaab9c0d97
apartigor <contact.jojigor@gmail.com>
2024-03-15 09:51:32 -0300

    Exercicio 4

---
+++ Programa/src/Exercicio04.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void executar() {
+       Scanner ler = new Scanner(System.in);
+
+       System.out.println("Digite 5 números:");
+       double[] numeros = new double[5];
+       for (int i = 0; i < 5; i++) {
+           numeros[i] = ler.nextDouble();
+       }
+
+       System.out.println("Digite um número n:");
+       double n = ler.nextDouble();
+
+       System.out.println("Os índices dos elementos inferiores a n são:");
+       for (int i = 0; i < 5; i++) {
+           if (numeros[i] < n) {
+               System.out.println(i + " ");
+           }
+       }
+
+       ler.close();
+   }
+}
